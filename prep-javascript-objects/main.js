908d3bcd6e3b7bb3e62a0978d58c6b0b31b02028
Matt Lang <matt26@yahoo.com>
2022-06-13 23:05:11 +0000

    Completed prep-javascript-objects exercise.

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,15 @@
+var person = {
+  firstName: 'Matthew',
+  lastName: 'Lang',
+  hobby: 'Hockey',
+  job: 'paramedic'
+};
+console.log('person:', person);
+var firstName = 'Matthew ';
+var lastName = 'Lang';
+var fullName = 'The persons name is ' + firstName + lastName + '.';
+console.log(fullName);
+person.job = 'paramedic';
+console.log('this person works as a', person.job);
+person.previous = 'Pirate';
+console.log('this person formerly worked as', person.previous);
