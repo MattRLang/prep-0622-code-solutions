4a74b55268207005176ea1ab0d0bb243c819c42c
Matt Lang <matt26@yahoo.com>
2022-06-14 02:38:50 +0000

    completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,17 @@
+var languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+console.log(languages);
+var firstElement = 'html';
+console.log('the first element of the array is', firstElement);
+var thirdElement = 'javascript';
+console.log('the third element of the array is', thirdElement);
+var length = 6;
+console.log('The length of the array', length);
+var lastElement = 'postgres';
+console.log('The last element of the array', lastElement);
